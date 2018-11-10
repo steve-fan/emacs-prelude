9f83c9f45ae669f4fbdb39d5f4c1ac5c9fd979c3
zhenchao <fanzc.daily@gmail.com>
2018-11-10 09:46:59 +0800

    配置 TSX 模式

--- modules/prelude-typescript.el
+++ modules/prelude-typescript.el
@@ -27,25 +27,40 @@
 ;;; Code:
 
 (require 'prelude-programming)
-(prelude-require-packages '(tide))
+(prelude-require-packages '(tide web-mode))
 
 (require 'typescript-mode)
 
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  (company-mode +1))
+
 (add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-mode))
 
-(with-eval-after-load 'typescript-mode
-  (defun prelude-ts-mode-defaults ()
-    (interactive)
-    (tide-setup)
-    (flycheck-mode +1)
-    (setq flycheck-check-syntax-automatically '(save mode-enabled))
-    (eldoc-mode +1)
-    (tide-hl-identifier-mode +1))
-  ;; formats the buffer before saving
-  (add-hook 'before-save-hook 'tide-format-before-save)
-  (setq prelude-ts-mode-hook 'prelude-ts-mode-defaults)
-  (add-hook 'typescript-mode-hook (lambda () (run-hooks 'prelude-ts-mode-hook))))
+(add-hook 'before-save-hook 'tide-format-before-save)
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
 
+(require 'web-mode)
+
+;; new marjor mode based on web-mode for TSX file
+(define-derived-mode tsx-mode web-mode "tsx"
+  "Major mode for editing typescript with react language code."
+  (message "tsx-mode loaded."))
+
+(defun highlight-element-and-column ()
+  "Highlight current HTML element and current column."
+  (setq-local web-mode-enable-current-element-highlight t)
+  (setq-local web-mode-enable-current-column-highlight t))
+
+(add-to-list 'auto-mode-alist '("\\.tsx\\'" . tsx-mode))
+(add-hook 'tsx-mode-hook 'emmet-mode)
+(add-hook 'tsx-mode-hook #'setup-tide-mode)
+(add-hook 'tsx-mode-hook #'highlight-element-and-column)
 
 (provide 'prelude-typescript)
 

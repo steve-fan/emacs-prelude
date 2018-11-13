e70c672d35788b71157f1a7da105e3bad8338834
zhenchao <fanzc.daily@gmail.com>
2018-11-13 22:43:34 +0800

    Add yasnippet support for all company backends

--- core/prelude-global-keybinding.el
+++ core/prelude-global-keybinding.el
@@ -37,6 +37,19 @@
   (define-key company-active-map (kbd "C-n") #'company-select-next)
   (define-key company-active-map (kbd "C-p") #'company-select-previous))
 
+;; Add yasnippet support for all company backends
+;; https://github.com/syl20bnr/spacemacs/pull/179
+(defvar company-mode/enable-yas t
+  "Enable yasnippet for all backends.")
+
+(defun company-mode/backend-with-yas (backend)
+  (if (or (not company-mode/enable-yas) (and (listp backend) (member 'company-yasnippet backend)))
+      backend
+    (append (if (consp backend) backend (list backend))
+            '(:with company-yasnippet))))
+
+(setq company-backends (mapcar #'company-mode/backend-with-yas company-backends))
+
 (package-install 'multiple-cursors)
 (global-set-key (kbd "C-c m a") 'mc/mark-all-like-this)
 (global-set-key (kbd "C-c m n") 'mc/mark-next-like-this)
